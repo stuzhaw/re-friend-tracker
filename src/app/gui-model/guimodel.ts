b62c92c38c21d214527926bd6037c72d0e0d0daf
stuzhaw <meierthi@students.zhaw.ch>
2021-12-11 09:54:16 +0000

    name geändert

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Thierry",
             "formList": [
                 {
                     "id": "FriendForm",
